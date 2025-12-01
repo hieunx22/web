4bef250472adcab19aaf8a9ad284035c510239c9
hieunx22 <hieunx22@uef.edu.vn>
2025-12-01 17:15:25 +0700

    update
    init project lap2

---
+++ lap2/lap2/Controllers/HelloWorldController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace lap2.Controllers
+{
+    public class HelloWorldController : Controller
+    {          
+       // public IActionResult Index()
+       // {
+       //     return View();
+        //}
+        //GET:/HelloWorld/
+        public string Index()
+
+        {
+            return "this is may default action...";
+        }
+        //GET:/HelloWorld/Welcome/
+        public string Welcome()
+        {
+            return " this is the Welcome action method...";
+        }
+    }
+}
